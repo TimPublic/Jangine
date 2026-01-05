6648ea2081826bc8238f23a245b5e40183881128
Revan1017 <timkloepper.public@gmail.com>
2026-01-05 03:39:14 +0100

    Created Mesh and QuadMesh

--- Jangine/src/main/java/internal/rendering/mesh/JangineMesh.java
+++ Jangine/src/main/java/internal/rendering/mesh/JangineMesh.java
@@ -1,6 +1,30 @@
 ﻿package internal.rendering.mesh;
 
 
-public class JangineMesh {
+import internal.rendering.JangineShaderProgram;
+
+
+// A mesh defines vertices and how to interpret them.
+// Those vertices consist of a position and a uv-coordinate.
+public abstract class JangineMesh {
+
+
+    protected float[] _vertices;
+    protected int[] _indices;
+
+    protected int _vaoID, _vboID, _eboID;
+
+
+    public int getVertexArrayObjectID() {
+        return _vaoID;
+    }
+
+    public int getVerticesSize() {
+        return _vertices.length;
+    }
+    public int getIndicesSize() {
+        return _indices.length;
+    }
+
 
 }
