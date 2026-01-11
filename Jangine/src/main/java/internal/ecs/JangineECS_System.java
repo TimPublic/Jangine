2b94763f0ecdccac513260c9e744a0051f75752c
Revan1017 <timkloepper.public@gmail.com>
2026-01-11 19:21:52 +0100

    Added ecs

---
+++ Jangine/src/main/java/internal/ecs/JangineECS_System.java
@@ -0,0 +1,130 @@
+﻿package internal.ecs;
+
+
+import internal.ecs.components.implementations.JangineECS_Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+
+/**
+ * This singleton class, accessible through the respective {@link JangineECS_System#get()} method,
+ * manages and updates all {@link JangineECS_Component} active.
+ * But it does not care about killing the {@link JangineECS_Component} upon removal (see {@link JangineECS_System#rmvComponents(List)}).
+ * This enables the system of "deactivating" entities or rather components and "activating" them again later on.
+ *
+ * @author Tim Kloepper
+ * @version 1.0
+ */
+public class JangineECS_System {
+
+
+    private HashSet<JangineECS_Component> _components;
+
+
+    private static JangineECS_System _instance;
+
+
+    private JangineECS_System() {
+        _components = new HashSet<>();
+    }
+
+    /**
+     * Returns the singleton instance of this system.
+     *
+     * @return the singleton instance of this system.
+     *
+     * @author Tim Kloepper
+     */
+    public static JangineECS_System get() {
+        if (_instance == null) {
+            _instance = new JangineECS_System();
+        }
+
+        return _instance;
+    }
+
+
+    // -+- UPDATE LOOP -+- //
+
+    /**
+     * Called every frame.
+     * Updates all registered {@link JangineECS_Component}.
+     *
+     * @param deltaTime time passed since the last frame
+     *
+     * @author Tim Kloepper
+     */
+    public final void update(double deltaTime) {
+        for (JangineECS_Component component : _components) {
+            component.update(deltaTime);
+        }
+    }
+
+
+    // -+- COMPONENT MANAGEMENT -+- //
+
+    /**
+     * Adds components to the system.
+     * These components need to be initialized, which typically
+     * happens when adding the components to a {@link JangineEntity}.
+     * If this condition is not met, the engine will crash.
+     *
+     * @param components components to be added
+     *
+     * @author Tim Kloepper
+     */
+    public void addComponents(Collection<JangineECS_Component> components) {
+        components.forEach(this::addComponent);
+    }
+    /**
+     * Adds a component to the system.
+     * This component needs to be initialized, which typically
+     * happens when adding a component to a {@link JangineEntity}.
+     * If this condition is not met, the engine will crash.
+     *
+     * @param component component to be added
+     *
+     * @author Tim Kloepper
+     */
+    public void addComponent(JangineECS_Component component) {
+        if (!component.hasBeenInitialized()) { // <- Has not been added by an entity.
+            System.err.println("[ECS ERROR] : Tried to add not initialized component!");
+            System.err.println("|-> Component : " + component);
+
+            System.exit(1);
+        }
+
+        _components.add(component);
+    }
+    /**
+     * Removes components from the system.
+     * The system does not call {@link JangineECS_Component#kill(JangineEntity)} on them.
+     * If you want to delete them because of the deletion of the {@link JangineEntity} that owns them,
+     * the {@link JangineEntity#kill()} method, already takes care of that.
+     *
+     * @param components components to be deleted
+     *
+     * @author Tim Kloepper
+     */
+    public void rmvComponents(Collection<JangineECS_Component> components) {
+        components.forEach(_components::remove); // IntelliJ suggestion
+    }
+    /**
+     * Removes a component from the system.
+     * The system does not call {@link JangineECS_Component#kill(JangineEntity)} on it.
+     * If you want to delete it because of the deletion of the {@link JangineEntity} that owns it,
+     * the {@link JangineEntity#kill()} method, already takes care of that.
+     *
+     * @param component component to be deleted
+     *
+     * @author Tim Kloepper
+     */
+    public void rmvComponent(JangineECS_Component component) {
+        _components.remove(component);
+    }
+
+
+}
---
+++ Jangine/src/main/java/internal/ecs/JangineEntity.java
@@ -0,0 +1,197 @@
+﻿package internal.ecs;
+
+
+import internal.ecs.components.implementations.JangineECS_Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+
+/**
+ * An entity is made out of {@link JangineECS_Component} that are updated every frame by {@link JangineECS_System}.
+ * The entity also contains a {@link HashSet} of ids, them being integers, for quick identification,
+ * for example of a player.
+ *
+ * @author Tim Klöpper
+ * @version 1.0
+ */
+public class JangineEntity {
+
+
+    private HashSet<JangineECS_Component> _components;
+    private HashSet<Integer> _ids;
+
+
+    public JangineEntity(List<Integer> ids) {
+        _components = new HashSet<>();
+        _ids = new HashSet<>(ids);
+    }
+
+
+    // -+- ID MANAGEMENT -+- //
+
+    /**
+     * Adds an id to the entity.
+     *
+     * @param id new id
+     *
+     * @author Tim Kloepper
+     */
+    public void addID(int id) {
+        _ids.add(id);
+    }
+
+    /**
+     * Removes an id from the entity.
+     * If the id does not exist, nothing happens.
+     *
+     * @param id id to be removed
+     *
+     * @author Tim Kloepper
+     */
+    public void rmvID(int id) {
+        _ids.remove(id);
+    }
+
+
+    // -+- COMPONENT MANAGEMENT -+- //
+
+    /**
+     * Adds a {@link Collection} {@link JangineECS_Component} to the entity.
+     *
+     * @param components components to be added
+     *
+     * @author Tim Kloepper
+     */
+    public void addComponents(Collection<JangineECS_Component> components) {
+        components.forEach(this::addComponent);
+    }
+    /**
+     * Adds a {@link JangineECS_Component} to the entity.
+     *
+     * @param component component to be added
+     *
+     * @author Tim Kloepper
+     */
+    public void addComponent(JangineECS_Component component) { // Thought about just passing the class, but if the subclass needs parameters this makes thing unnecessarily complicated.
+        component.init(this);
+
+        JangineECS_System.get().addComponent(component);
+    }
+    /**
+     * Removes a {@link Collection} {@link JangineECS_Component} from the entity.
+     * If the components do not belong to the entity,
+     * nothing happens.
+     *
+     * @param components components to be removed
+     *
+     * @author Tim Kloepper
+     */
+    public void rmvComponents(Collection<JangineECS_Component> components) {
+        components.forEach(this::rmvComponent);
+    }
+    /**
+     * Removes a {@link JangineECS_Component} from the entity.
+     * If the component does not belong to the entity,
+     * nothing happens.
+     *
+     * @param component component to be removed
+     *
+     * @author Tim Kloepper
+     */
+    public void rmvComponent(JangineECS_Component component) {
+        if (!_components.contains(component)) {
+            return;
+        }
+
+        component.kill(this);
+
+        JangineECS_System.get().rmvComponent(component);
+    }
+
+
+    // -+- LIFE CYCLE -+- //
+
+    /**
+     * Called upon the deletion of this entity.
+     * Clears the components and deletes them properly,
+     * also from {@link JangineECS_System}.
+     *
+     * @author Tim Kloepper
+     */
+    public void kill() {
+        JangineECS_System.get().rmvComponents(_components);
+
+        for (JangineECS_Component component : _components) {
+            component.kill(this);
+        }
+
+        _components.clear();
+    }
+
+
+    // -+- GETTERS -+- //
+
+    /**
+     * Returns all {@link JangineECS_Component}, that this entity owns.
+     *
+     * @return all components
+     *
+     * @author Tim Kloepper
+     */
+    public HashSet<JangineECS_Component> getComponents() {
+        return _components;
+    }
+
+
+    // -+- CHECKERS -+- //
+
+    /**
+     * Returns true, if the entity contains the specified id.
+     * False, if the entity does not contain the specified id.
+     *
+     * @param id id to be checked
+     *
+     * @return whether the entity contains the id or not
+     *
+     * @author Tim Kloepper
+     */
+    public boolean hasID(int id) {
+        return _ids.contains(id);
+    }
+
+    /**
+     * Checks, if the entity has a {@link JangineECS_Component} of the specified class.
+     *
+     * @param type class of the component
+     *
+     * @return whether the entity has a component of that class or not
+     *
+     * @author Tim Kloepper
+     */
+    public boolean hasComponentOfType(Class<? extends JangineECS_Component> type) {
+        for (JangineECS_Component component : _components) {
+            if (type.isInstance(component)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    /**
+     * Checks if the entity has the specified {@link JangineECS_Component}.
+     *
+     * @param component component to be checked
+     *
+     * @return whether the entity has the specified component or not
+     *
+     * @author Tim Kloepper
+     */
+    public boolean hasComponent(JangineECS_Component component) {
+        return _components.contains(component);
+    }
+
+
+}
