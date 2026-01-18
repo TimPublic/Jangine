066e1e048a4fb86df9a1a930c3cb5344c97c7935
Revan1017 <timkloepper.public@gmail.com>
2026-01-18 14:34:36 +0100

    Removed unnecessary imports.

--- Jangine/src/main/java/Main.java
+++ Jangine/src/main/java/Main.java
@@ -1,8 +1,3 @@
-import internal.ecs.*;
-import internal.ecs.specific.movement.MovementComponent;
-import internal.ecs.specific.movement.MovementComponentSystem;
-import internal.ecs.specific.position.PositionComponent;
-import internal.ecs.specific.position.PositionComponentSystem;
 import internal.main.Engine;
 
 
