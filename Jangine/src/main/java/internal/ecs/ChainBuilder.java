f05b80a64e6c2bf8bcd7a3e39418b13fee375d1f
Revan1017 <timkloepper.public@gmail.com>
2026-01-19 21:00:16 +0100

    Added a chain builder to the entity component system.

---
+++ Jangine/src/main/java/internal/ecs/ChainBuilder.java
@@ -0,0 +1,97 @@
+﻿package internal.ecs;
+
+
+public class ChainBuilder {
+
+
+    private ECS _ecs;
+    private int _entityID;
+
+
+    public ChainBuilder start(ECS system) {
+        if (system == null) {return null;}
+        if (_entityID != -1) {
+            System.err.println("[CHAIN BUILDER ERROR] : Already building an entity!");
+            System.err.println("|-> Current entity : " + _entityID);
+
+            System.exit(1);
+        }
+
+        _ecs = system;
+        _entityID = _ecs.addEntity();
+
+        return this;
+    }
+    public ChainBuilder start(ECS system, int entityID) {
+        if (system == null) {return null;}
+        if (_entityID != -1) {
+            System.err.println("[CHAIN BUILDER ERROR] : Already building an entity!");
+            System.err.println("|-> Current entity : " + _entityID);
+
+            System.exit(1);
+        }
+
+        _ecs = system;
+        _entityID = entityID;
+
+        return this;
+    }
+    public int finish() {
+        int id;
+
+        id = _entityID;
+
+        _ecs = null;
+        _entityID = -1;
+
+        return id;
+    }
+
+    public ChainBuilder add(ECS_Component component) {
+        if (_entityID == -1) {
+            System.err.println("[CHAIN BUILDER ERROR] : Initialization not started!");
+
+            System.exit(1);
+        }
+
+        _ecs.addComponent(_entityID, component);
+
+        return this;
+    }
+    public ChainBuilder addSys(ECS_ComponentSystem<?> componentSystem, Class<? extends ECS_Component> componentClass) {
+        if (_entityID == -1) {
+            System.err.println("[CHAIN BUILDER ERROR] : Initialization not started!");
+
+            System.exit(1);
+        }
+
+        _ecs.addComponentSystem(componentSystem, componentClass);
+
+        return this;
+    }
+
+    public ChainBuilder rmv(ECS_Component component) {
+        if (_entityID == -1) {
+            System.err.println("[CHAIN BUILDER ERROR] : Initialization not started!");
+
+            System.exit(1);
+        }
+
+        _ecs.rmvComponent(_entityID, component);
+
+        return this;
+    }
+    public ChainBuilder rmvSys(Class<? extends ECS_Component> componentClass) {
+        if (_entityID == -1) {
+            System.err.println("[CHAIN BUILDER ERROR] : Initialization not started!");
+
+            System.exit(1);
+        }
+
+        _ecs.rmvComponentSystem(componentClass);
+
+        return this;
+    }
+
+
+}
