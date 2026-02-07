f2b78b265272f8b0e5212158eb41a7d590499772
Revan1017 <timkloepper.public@gmail.com>
2026-02-07 13:54:42 +0100

    Implemented sound classes without actual implementation.

---
+++ Jangine/src/main/java/internal/audio/Sound.java
@@ -0,0 +1,53 @@
+﻿package internal.audio;
+
+
+import internal.util.FormatChecker;
+
+
+public class Sound {
+
+
+    // -+- CREATION -+- //
+
+    public Sound(String path) {
+        FormatChecker.assertFormat(path, ".wav");
+
+        _SOURCE_ID = h_genSource();
+        _PATH = path;
+    }
+
+    private long h_genSource() {
+        return -1L;
+    }
+
+
+    // -+- PARAMETERS -+- //
+
+    // FINALS //
+
+    private final long _SOURCE_ID;
+    private final String _PATH;
+
+
+    // -+- SOUND MANAGEMENT -+- //
+
+    public void play() {
+
+    }
+    public void stop() {
+
+    }
+
+
+    // -+- GETTERS -+- //
+
+    public int getLenghtMin() {
+        return -1;
+    }
+
+    public String getPath() {
+        return _PATH;
+    }
+
+
+}
---
+++ Jangine/src/main/java/internal/audio/SoundManager.java
@@ -0,0 +1,71 @@
+﻿package internal.audio;
+
+
+import java.util.HashMap;
+
+
+public class SoundManager {
+
+
+    // -+- CREATION -+- //
+
+    public SoundManager() {
+        _SOUNDS = new HashMap<>();
+    }
+
+
+    // -+- PARAMETERS -+- //
+
+    // FINALS //
+
+    private final HashMap<String, Sound> _SOUNDS;
+
+
+    // -+- SOUND MANAGEMENT -+- //
+
+    // LOADING //
+
+    public Sound load(String path) {
+        Sound sound;
+
+        sound = _SOUNDS.get(path);
+        if (sound == null) {
+            sound = new Sound(path);
+            _SOUNDS.put(path, sound);
+        }
+
+        return sound;
+    }
+
+    // ADDITION AND REMOVAL //
+
+    public boolean add(String path) {
+        load(path);
+
+        return true;
+    }
+    public boolean add(Sound sound) {
+        String path;
+        Sound foundSound;
+
+        path = sound.getPath();
+        foundSound = _SOUNDS.get(path);
+
+        if (foundSound == null) {
+            _SOUNDS.put(path, sound);
+
+            return true;
+        }
+
+        return foundSound == sound;
+    }
+
+    public boolean rmv(String path) {
+        return (_SOUNDS.remove(path) != null);
+    }
+    public boolean rmv(Sound sound) {
+        return _SOUNDS.remove(sound.getPath(), sound);
+    }
+
+
+}
